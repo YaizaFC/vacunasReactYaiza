27c175bd714056b2425a89d1546370de8d6327a8
yaizaFC <yaizafritis@gmail.com>
2021-02-24 14:03:01 +0100

    estillos datos globales

--- src/Components/DatosGlobales.js
+++ src/Components/DatosGlobales.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/estilos.css';
+import imagenes from '../assets/imagenes'
 import axios from 'axios';
 
 function Datos_globales() {
@@ -10,6 +11,8 @@ function Datos_globales() {
     const [contador, setContador] = useState(0);
     const [vacunasPFizerTotales, setVacunasPFizerTotales] = useState(0);
     const [dosisEntregadas, setDosisEntregadas] = useState(0);
+    const [dosisEntregadasP, setDosisEntregadasP] = useState(0);
+    const [dosisEntregadasM, setDosisEntregadasM] = useState(0);
     const [dosisAdministradas, setDosisAdministradas] = useState(0);
     const [pautaCompleta, setPautaCompleta] = useState(0);
     const peticionGet = async () => {
@@ -24,12 +27,14 @@ function Datos_globales() {
                     vacunasPFizer = vacunasPFizer + vacuna.dosisPfizer; //Contador de todas las vacunas Pfizer
                     vacunasModerna = vacunasModerna + vacuna.dosisModerna; //Contador de todas las vacunas Moderna
                     dosisAdm = dosisAdm + vacuna.dosisAdministradas;
-                    pautaComp=pautaComp+vacuna.personasCompleta;
+                    pautaComp = pautaComp + vacuna.personasCompleta;
 
                 });
 
                 //setVacunasPFizerTotales(vacunasPFizer);
-                setDosisEntregadas(vacunasModerna+vacunasPFizer);
+                setDosisEntregadasP(vacunasPFizer);
+                setDosisEntregadasM(vacunasModerna);
+                setDosisEntregadas(vacunasModerna + vacunasPFizer);
                 setDosisAdministradas(dosisAdm);
                 setPautaCompleta(pautaComp);
 
@@ -46,13 +51,19 @@ function Datos_globales() {
     return (
         <div class="album py-5 bg-light">
             <div class="container">
+
+                <h1 class="titulo1">Situación Actual</h1>
+                <img src={imagenes.img4} alt="imagen virus" class="imgVirus" ></img>
+
+                <h3 class="titulo3 sep">Datos Globales</h3>
+                <hr />
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                     <div className="col">
                         <div class="card shadow-sm">
                             <svg class="bd-placeholder-img card-img-top" width="100%" height="70" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#C5FFDA" /><text x="10%" y="50%" fill="#000000" dy=".3em">Vacunas Totales Entregadas en CCAA</text></svg>
 
                             <div class="card-body">
-                                <p class="card-text grande">{dosisEntregadas}</p>
+                                <img src={imagenes.img5} alt="imagen comunidad autonoma" class="inline imgCuadro1" ></img><p class="card-text grande inline">{dosisEntregadas}</p><img src={imagenes.img5} alt="imagen vacuna" class="inline imgCuadro1" ></img>
                             </div>
                         </div>
                     </div>
@@ -61,9 +72,9 @@ function Datos_globales() {
                             <svg class="bd-placeholder-img card-img-top" width="100%" height="70" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#C5FFDA" /><text x="30%" y="50%" fill="#000000" dy=".3em">Dosis Administradas</text></svg>
 
                             <div class="card-body">
-                                <p class="card-text">{dosisAdministradas}</p>
-                                <hr/>
-                                <p class="card-text">{Math.round((dosisAdministradas*100)/dosisEntregadas)}% dosis recibidas</p>
+                                <img src={imagenes.img6} alt="imagen vacuna" class="inline imgCuadro2" ></img><p class="card-text inline">{dosisAdministradas}</p><img src={imagenes.img6} alt="imagen vacuna" class="inline imgCuadro2" ></img>
+                                <hr />
+                                <p class="card-text">{Math.round((dosisAdministradas * 100) / dosisEntregadas)}% dosis recibidas</p>
                             </div>
                         </div>
                     </div>
@@ -72,15 +83,62 @@ function Datos_globales() {
                             <svg class="bd-placeholder-img card-img-top" width="100%" height="70" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#C5FFDA" /><text x="15%" y="50%" fill="#000000" dy=".3em">Nº de personas con pauta Completa</text></svg>
 
                             <div class="card-body">
-                                <p class="card-text">{pautaCompleta}</p>
-                                <hr/>
-                                <p class="card-text">{Math.round((pautaCompleta*100)/dosisAdministradas)}% dosis administradas</p>
+                                <img src={imagenes.img7} alt="imagen personas pauta completa" class="inline imgCuadro3" ></img><p class="card-text inline">{pautaCompleta}</p><img src={imagenes.img7} alt="imagen vacuna" class="inline imgCuadro3" ></img>
+                                <hr />
+                                <p class="card-text">{Math.round((pautaCompleta * 100) / dosisAdministradas)}% dosis administradas</p>
                             </div>
                         </div>
                     </div>
                 </div>
+                <h3 class="titulo3">Distribución por tipo de vacuna</h3>
+                <hr />
+                <table class="tablaDatosGlobales">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Dosis entregadas en CCAA<hr /></th>
+                            <th>Nº personas con pauta completa<hr /></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><span class="dosisEntregadasPfizer">Pfizer</span></td>
+                            <td>{dosisEntregadasP}</td>
+                            <td>{pautaCompleta}</td>
+                        </tr>
+                        <tr>
+                            <td><span class="dosisEntregadasModerna">Moderna</span></td>
+                            <td>{dosisEntregadasM}</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td><span class="noDatos">AstraZeneca / Oxford</span></td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td><span class="noDatos">Janssen</span></td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                        <tr>
+                            <td><span class="noDatos">Sanofi Pasteur/GSK</span></td>
+                            <td>-</td>
+                            <td>-</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
             </div>
+            <div>
+                <p class="inline info">Informe de actividad diario. GIV – Gestión Integral de vacunación frente al COVID-19 en España</p>
+                <img src={imagenes.img8} alt="Gobierno de españa - Ministerio de salud" class="right imgGobierno" />
+            </div>
+            <hr class="pie"/>
+            <p class="info2">Yaiza Fritis Calvo - Despliegue de Aplicaciones Web - 2020/21</p>
         </div>
+
     );
 
 }
--- src/assets/imagenes.js
+++ src/assets/imagenes.js
@@ -1,10 +1,20 @@
 import img_usuarios from'./img/usuarios.png'
 import img_vacunacion from'./img/vacunacion.jpg'
 import img_vacuna_covid from'./img/vacuna_covid.png'
+import img_virus from'./img/virus.png'
+import img_ccaa from'./img/ccaa.png'
+import img_vacuna from'./img/vacuna.png'
+import img_persona from'./img/persona.png'
+import img_gobierno from'./img/gobSalud.png'
 //import img_vacunacion from'../assets/imagenes/vacunacion.jpg'
 //aui irian todas las imagenes que quisieramos utilizar
 export default{
     "img1":img_usuarios,
     "img2":img_vacunacion,
-    "img3":img_vacuna_covid
+    "img3":img_vacuna_covid,
+    "img4":img_virus,
+    "img5":img_ccaa,
+    "img6":img_vacuna,
+    "img7":img_persona,
+    "img8":img_gobierno
 }
