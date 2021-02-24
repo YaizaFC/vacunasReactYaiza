0d179948b3648da3421c8e2a9d386c3b2784d591
yaizaFC <yaizafritis@gmail.com>
2021-02-24 18:22:12 +0100

    cosas

---
+++ build/precache-manifest.a2c21dfe8970b1e06bdc4107adf1958c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f871bb3cc7736ef797fdef3b06b258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5951d6337a5359e7268f",
+    "url": "/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "371599c776b5ff6b29a5",
+    "url": "/static/css/main.e6d3f71a.chunk.css"
+  },
+  {
+    "revision": "5951d6337a5359e7268f",
+    "url": "/static/js/2.e54d5d79.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/static/js/2.e54d5d79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "371599c776b5ff6b29a5",
+    "url": "/static/js/main.79a22d07.chunk.js"
+  },
+  {
+    "revision": "ac3c74821b41c258d6e4",
+    "url": "/static/js/runtime-main.32b585e6.js"
+  },
+  {
+    "revision": "426338c0c5fb5baab2fb40efcc4f5408",
+    "url": "/static/media/ccaa.426338c0.png"
+  },
+  {
+    "revision": "7e0266280882f758e6fdea1446f0b1cc",
+    "url": "/static/media/gobSalud.7e026628.png"
+  },
+  {
+    "revision": "0dfb67594f8a6572708a0e583850486d",
+    "url": "/static/media/persona.0dfb6759.png"
+  },
+  {
+    "revision": "a4d1530bb62dcef039fbffc49f07b7d7",
+    "url": "/static/media/usuarios.a4d1530b.png"
+  },
+  {
+    "revision": "b2bfab1422a8f085ac22f11ebf156565",
+    "url": "/static/media/vacuna_covid.b2bfab14.png"
+  },
+  {
+    "revision": "b589fa54aebcda9b44e4351870f90eee",
+    "url": "/static/media/vacunacion.b589fa54.jpg"
+  },
+  {
+    "revision": "7242f80c27b85da0b250324901c1de82",
+    "url": "/static/media/virus.7242f80c.png"
+  }
+]);
